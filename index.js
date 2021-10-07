b8d6225fc3d6f9fa2501af1ff9058a897a208d46
IgnasDav <ignas321@gmail.com>
2021-10-07 21:45:07 +0300

    Adding a favorite button

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const nameInput = document.querySelector("#name");
 const telInput = document.querySelector("#tel");
 const btnAdd = document.querySelector(".main__form__btn");
 const mainForm = document.querySelector(".main__form");
-const listDiv = document.createElement("div");
+const addressDiv = document.createElement("div");
 const arr = [];
 
 //Inserting items into html
 
-mainForm.append(listDiv);
+mainForm.append(addressDiv);
 const emptyMsg = document.createElement("h2");
 emptyMsg.textContent = "Please Input A Value";
 
@@ -19,51 +19,79 @@ emptyMsg.textContent = "Please Input A Value";
 function drawAddress() {
   // //Adding and inserting html elements
 
-  const list = document.createElement("ul");
-  listDiv.append(list);
-  const listItem1 = document.createElement("li");
-  const listItem2 = document.createElement("li");
-
+  addressDiv.innerHTML = null;
   emptyMsg.remove();
 
   //Looping through array to add single address
 
-  arr.forEach((singleItem) => {
+  arr.forEach((singleItem, i) => {
+    const div = document.createElement("div");
+    div.classList.add("main__form__div");
+    const listItem1 = document.createElement("p");
+    const listItem2 = document.createElement("p");
     if (isNaN(singleItem.name)) {
       listItem1.innerHTML = null;
-      list.append(listItem1);
       listItem1.append(singleItem.name);
+      div.append(listItem1);
+      addressDiv.append(div);
     }
     if (!isNaN(singleItem.tel)) {
       listItem2.innerHTML = null;
-      list.append(listItem2);
+      div.append(listItem2);
       listItem2.append(singleItem.tel);
     }
+    //Creating div for delete,add favorite and edit
+    const btnDiv = document.createElement("div");
+    btnDiv.classList.add("main__form__btnDiv");
+
     //Delete function
     const btnDelete = document.createElement("button");
     btnDelete.textContent = "Delete";
-    listItem2.append(btnDelete);
+    btnDiv.append(btnDelete);
+    div.append(btnDiv);
     btnDelete.addEventListener("click", (event) => {
       event.preventDefault();
       // delete singleItem.name;
       // delete singleItem.tel;
-      arr.splice(singleItem, 1);
+      arr.splice(i, 1);
       drawAddress();
     });
-    // if (!singleItem.name && !singleItem.tel) {
-    //   list.remove();
-    // }
+
+    //Adding item as favorite
+
+    const favorite = document.createElement("INPUT");
+    favorite.setAttribute("type", "checkbox");
+    favorite.classList.add("main__form__checkbox");
+    btnDiv.append(favorite);
+    favorite.addEventListener("input", (event) => {
+      if (favorite) {
+        arr.push({ favorite: true });
+      }
+      if (!favorite) {
+        arr.push({ favorite: false });
+      }
+    });
+
+    //Adding an edit feature
+
+    const edit = document.createElement("button");
+    btnDiv.append(edit);
+    edit.textContent = "Edit";
   });
 }
 
 btnAdd.addEventListener("click", (event) => {
   event.preventDefault();
+
   const nameInputValue = nameInput.value;
   const telInputValue = telInput.value;
   if (nameInputValue === "" || telInputValue === "") {
     mainForm.append(emptyMsg);
   } else {
-    arr.push({ name: nameInputValue, tel: telInputValue });
+    arr.push({
+      name: nameInputValue,
+      tel: telInputValue,
+    });
     drawAddress();
   }
 });
