3755726bdefa9e7247707e246255ecb10f4ca58c
IgnasDav <ignas321@gmail.com>
2021-10-07 19:29:28 +0300

    In the process of adding a delete button to the addresses

--- index.js
+++ index.js
@@ -1,45 +1,68 @@
 "use strict";
+
 //Global variables
+
 const nameInput = document.querySelector("#name");
 const telInput = document.querySelector("#tel");
 const btnAdd = document.querySelector(".main__form__btn");
 const mainForm = document.querySelector(".main__form");
-const list = document.createElement("ul");
-//Inserting items into html
-mainForm.append(list);
-
+const listDiv = document.createElement("div");
 const arr = [];
-const reducedArr = {};
+
+//Inserting items into html
+
+mainForm.append(listDiv);
+const emptyMsg = document.createElement("h2");
+emptyMsg.textContent = "Please Input A Value";
+
+//Drawing function
 function drawAddress() {
+  //Adding and inserting html elements
+
   const nameInputValue = nameInput.value;
   const telInputValue = telInput.value;
-  const listItem1 = document.createElement("li");
-  const listItem2 = document.createElement("li");
-  arr.push(nameInputValue);
-  arr.push(telInputValue);
-  arr.reduce(
-    (result, singleItem) => {
-      if (isNaN(singleItem)) {
-        result.names.push(singleItem);
+
+  //Checking the  values if they are empty
+
+  if (nameInputValue === "" || telInputValue === "") {
+    mainForm.append(emptyMsg);
+  }
+  //If the values are not empty then we add the address list
+  else {
+    // //Adding and inserting html elements
+
+    const list = document.createElement("ul");
+    listDiv.append(list);
+    const listItem1 = document.createElement("li");
+    const listItem2 = document.createElement("li");
+    const btnDelete = document.createElement("button");
+    btnDelete.textContent = "Delete";
+    emptyMsg.remove();
+
+    //Pushing values into the array object
+
+    arr.push({ name: nameInputValue, tel: telInputValue });
+
+    //Looping through array to add single address
+
+    arr.forEach((singleItem) => {
+      if (isNaN(singleItem.name)) {
         listItem1.innerHTML = null;
         list.append(listItem1);
-        listItem1.append(singleItem);
+        listItem1.append(singleItem.name);
       }
-      if (!isNaN(singleItem)) {
-        result.phones.push(singleItem);
+      if (!isNaN(singleItem.tel)) {
         listItem2.innerHTML = null;
         list.append(listItem2);
-        listItem2.append(singleItem);
+        listItem2.append(singleItem.tel);
       }
-      return result;
-    },
-    { names: [], phones: [] }
-  );
-  //   reducedArr.forEach((singleItem) => {
-  //     listItem.innerHTML = null;
-  //     list.append(listItem);
-  //     listItem.append(singleItem);
-  //   });
+      list.append(btnDelete);
+    });
+    btnDelete.addEventListener("click", (event) => {
+      event.preventDefault();
+      arr.shift();
+    });
+  }
 }
 
 btnAdd.addEventListener("click", (event) => {
