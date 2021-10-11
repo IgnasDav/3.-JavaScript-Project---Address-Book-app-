9b0d55fd7fb423d623ea4123fc00fe9544c04e40
IgnasDav <ignas321@gmail.com>
2021-10-11 18:50:03 +0300

    Error: Too much recursion

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const btnAdd = document.querySelector(".main__form__btn");
 const mainForm = document.querySelector(".main__form");
 const addressDiv = document.createElement("div");
 const arr = [];
+const searchInput = document.querySelector("#search");
+// const nameInput
 
 //Inserting items into html
 
@@ -23,63 +25,79 @@ function drawAddress() {
   emptyMsg.remove();
 
   //Looping through array to add single address
-
-  arr.forEach((singleItem, i) => {
-    const div = document.createElement("div");
-    div.classList.add("main__form__div");
-    const listItem1 = document.createElement("p");
-    const listItem2 = document.createElement("p");
-    if (isNaN(singleItem.name)) {
-      listItem1.innerHTML = null;
-      listItem1.append(singleItem.name);
-      div.append(listItem1);
-      addressDiv.append(div);
-    }
-    if (!isNaN(singleItem.tel)) {
-      listItem2.innerHTML = null;
-      div.append(listItem2);
-      listItem2.append(singleItem.tel);
-    }
-    //Creating div for delete,add favorite and edit
-    const btnDiv = document.createElement("div");
-    btnDiv.classList.add("main__form__btnDiv");
-
-    //Delete function
-    const btnDelete = document.createElement("button");
-    btnDelete.textContent = "Delete";
-    btnDiv.append(btnDelete);
-    div.append(btnDiv);
-    btnDelete.addEventListener("click", (event) => {
-      event.preventDefault();
-      // delete singleItem.name;
-      // delete singleItem.tel;
-      arr.splice(i, 1);
-      drawAddress();
-    });
-
-    //Adding item as favorite
-
-    const favorite = document.createElement("INPUT");
-    favorite.setAttribute("type", "checkbox");
-    favorite.classList.add("main__form__checkbox");
-    btnDiv.append(favorite);
-    favorite.addEventListener("input", (event) => {
-      if (favorite) {
-        arr.push({ favorite: true });
+  const filteredArr = arr
+    .filter((address) => {
+      if (
+        address.name.toLowerCase().includes(searchInput.value) ||
+        address.tel.toLowerCase().includes(searchInput.value)
+      ) {
+        return drawAddress();
       }
-      if (!favorite) {
-        arr.push({ favorite: false });
+    })
+    .forEach((singleItem, i) => {
+      const div = document.createElement("div");
+      div.classList.add("main__form__div");
+      const listItem1 = document.createElement("p");
+      const listItem2 = document.createElement("p");
+      if (isNaN(singleItem.name)) {
+        listItem1.innerHTML = null;
+        listItem1.append(singleItem.name);
+        div.append(listItem1);
+        addressDiv.append(div);
       }
+      if (!isNaN(singleItem.tel)) {
+        listItem2.innerHTML = null;
+        div.append(listItem2);
+        listItem2.append(singleItem.tel);
+      }
+      //Creating div for delete,add favorite and edit
+      const btnDiv = document.createElement("div");
+      btnDiv.classList.add("main__form__btnDiv");
+
+      //Delete function
+      const btnDelete = document.createElement("button");
+      btnDelete.textContent = "Delete";
+      btnDiv.append(btnDelete);
+      div.append(btnDiv);
+      btnDelete.addEventListener("click", (event) => {
+        event.preventDefault();
+        // delete singleItem.name;
+        // delete singleItem.tel;
+        arr.splice(i, 1);
+        drawAddress();
+      });
+
+      //Adding item as favorite
+
+      const favorite = document.createElement("INPUT");
+      favorite.setAttribute("type", "checkbox");
+      favorite.classList.add("main__form__checkbox");
+      btnDiv.append(favorite);
+      favorite.addEventListener("input", () => {
+        singleItem.favorite = true;
+      });
+
+      //Adding an edit feature
+
+      const edit = document.createElement("button");
+      btnDiv.append(edit);
+      edit.textContent = "Edit";
+      edit.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (event) {
+          singleItem.name = nameInput.value;
+          singleItem.tel = telInput.value;
+          // const editNameInput = document.createElement("input");
+          // editNameInput.setAttribute("type", "text");
+          // const editTelInput = document.createElement("input");
+          // editTelInput.setAttribute("type", "tel");
+          // singleItem.name = editNameInput;
+          // singleItem.tel = editTelInput;
+          drawAddress();
+        }
+      });
     });
-
-    //Adding an edit feature
-
-    const edit = document.createElement("button");
-    btnDiv.append(edit);
-    edit.textContent = "Edit";
-  });
 }
-
 btnAdd.addEventListener("click", (event) => {
   event.preventDefault();
 
@@ -91,10 +109,8 @@ btnAdd.addEventListener("click", (event) => {
     arr.push({
       name: nameInputValue,
       tel: telInputValue,
+      favorite: false,
     });
     drawAddress();
   }
 });
-// btnDelete.addEventListener("click", (event) => {
-//   event.preventDefault();
-// });
